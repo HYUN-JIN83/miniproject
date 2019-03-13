18a4b04cc45fdee9529b8e00042284721d737813
KIM HYUN JIN <hjkimt@gmail.com>
2019-03-13 17:40:00 +0900

    frist routing connection

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from 'express'
+import {router} from './routes/admin'
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
     res.send('first app')
 })
+
+app.use('/admin', router)
 
 app.listen(port, () => {
     console.log('Server Running')
