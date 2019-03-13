9b6e17692f32837fea1459288644d9a7bd5d599c
KIM HYUN JIN <hjkimt@gmail.com>
2019-03-13 18:08:35 +0900

    mongoose

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ import express from 'express'
 import {router} from './routes/admin'
 import path from 'path'
 import helmet from 'helmet'
+import mongoose from 'mongoose'
+
+const db = mongoose.connection
+db.on('error', console.error)
+db.once('open', () => {
+    console.log('mongoDB connect')
+})
+mongoose.connect('mongodb://127.0.0.1:27017/miniproject', { useMongoClient: true })
+
 
 const app = express()
 const port = 3000
