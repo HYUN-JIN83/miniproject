01419a219e30f600bb5eff0956f0a77a026d24f6
KIM HYUN JIN <hjkimt@gmail.com>
2019-03-16 17:56:33 +0900

    join

--- libs/passwordHash.js
+++ libs/passwordHash.js
@@ -1,6 +1,7 @@
 import crypto from 'crypto'
 const mysalt = 'hjkimt'
 
-const password = (password) => {
-    return crypto.createHash('sha512').update(password+mysalt).digest('base64')
+
+module.exports = (password) => {
+    return crypto.createHash('sha512').update( password + mysalt).digest('base64')
 }
